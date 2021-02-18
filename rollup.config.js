ae62138a0becf5e2f136634547ddd8b0bc2ffe68
Tim Kye <Timothy.Kye@nike.com>
2021-02-17 16:17:27 -0800

    bundle deps with rollup to avoid shipping the entire @aws-sdk

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,9 @@
 // rollup.config.js
 import typescript from '@rollup/plugin-typescript'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/main.ts',
@@ -7,6 +11,6 @@ export default {
     dir: 'lib',
     format: 'cjs',
   },
-  external: ['@aws-sdk/client-dynamodb', '@aws-sdk/util-dynamodb'],
-  plugins: [typescript()],
+  // external: ['@aws-sdk/client-dynamodb', '@aws-sdk/util-dynamodb'],
+  plugins: [typescript(), nodeResolve({ jsnext: 'main' }), commonjs(), json(), terser()],
 }
