bb5b749f0529406081f7317a9c6689971557cc31
Tim Kye <Timothy.Kye@nike.com>
2021-08-03 09:31:36 -0700

    remove v2 lib dir

--- lib/types/logger.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Logger {
-    info: (...args: unknown[]) => void;
-    error: (...args: unknown[]) => void;
-    warn: (...args: unknown[]) => void;
-    debug: (...args: unknown[]) => void;
-    minimumLogLevel?: string;
-}
-export declare function wrapper(logger?: unknown): Logger;
